1023d8884b6cdddfa08e9ccded464c454ac22d0f
AlaaShalabe <al1996sh.9@gmail.com>
2021-11-01 23:14:29 +0200

    creat index Route

--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,5 @@ Route::group(['middleware' => 'auth'], function () {
     Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
     Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
+
+Route::get('admin/product/index',[AdminController::class,'index'])->name('admin.index');
