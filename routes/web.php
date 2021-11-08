9a3dd4c10865ac17381c07a27ab50819bf4bc904
nasser650 <85414269+nasser650@users.noreply.github.com>
2021-11-09 00:59:39 +0300

    convert ecomerce html template to blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 */
 
-Route::view('/', 'welcome');
+Route::view('/', 'public.store');
 
 Auth::routes();
 
