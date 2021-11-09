a448557663d20442a56c8848707041cdd98d25f3
nasser650 <85414269+nasser650@users.noreply.github.com>
2021-11-09 14:57:01 +0300

    fix sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,104 +1,105 @@
-<div class="sidebar" data-color="orange" data-background-color="white" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
-  <!--
+<div class="sidebar" data-color="orange" data-background-color="white"
+    data-image="{{ asset('material') }}/img/sidebar-1.jpg">
+    <!--
       Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
       Tip 2: you can also add an image using data-image tag
   -->
-  <div class="logo">
-    <a href="https://creative-tim.com/" class="simple-text logo-normal">
-      {{ __('Admin') }}
-    </a>
-  </div>
-  <div class="sidebar-wrapper">
-    <ul class="nav">
-      <li class="nav-item {{ ($activePage == 'financial-statement' || $activePage == 'user-management' || $activePage == 'delivery-companies') ? ' active' : '' }}">
-        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
-          <i><img style="width:25px" src="{{ asset('material') }} https://www.seekpng.com/png/detail/85-856344_dashboard-icon-monitoring-and-control-icon.png"></i>
-          <!-- <i><img style="width:25px" href="https://www.seekpng.com/png/detail/85-856344_dashboard-icon-monitoring-and-control-icon.png"></i> -->
-          <p>{{ __('Dashboard') }}
-            <b class="caret"></b>
-          </p>
+    <div class="logo">
+        <a href="https://creative-tim.com/" class="simple-text logo-normal">
+            {{ __('Admin') }}
         </a>
-        <div class="collapse show" id="laravelExample">
-          <ul class="nav">
-            <li class="nav-item{{ $activePage == 'financial-statement' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('profile.edit') }}">
-                <span class="sidebar-normal">{{ __('Financial Statement') }} </span>
-              </a>
-            </li>
-            <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('user.index') }}">
-                <span class="sidebar-normal"> {{ __('User Management') }} </span>
-              </a>
-            </li>
-            <li class="nav-item{{ $activePage == 'delivery-companies' ? ' active' : '' }}">
-                <a class="nav-link" href="{{ route('admin.delivery') }}">
-                  <span class="sidebar-normal"> {{ __('Delivery Companies') }} </span>
+    </div>
+    <div class="sidebar-wrapper">
+        <ul class="nav">
+            <li
+                class="nav-item {{ $activePage == 'financial-statement' || $activePage == 'user-management' || $activePage == 'delivery-companies' ? ' active' : '' }}">
+                <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
+                    <i><img style="width:25px"
+                            src="{{ asset('material') }} https://www.seekpng.com/png/detail/85-856344_dashboard-icon-monitoring-and-control-icon.png"></i>
+                    <!-- <i><img style="width:25px" href="https://www.seekpng.com/png/detail/85-856344_dashboard-icon-monitoring-and-control-icon.png"></i> -->
+                    <p>{{ __('Dashboard') }}
+                        <b class="caret"></b>
+                    </p>
                 </a>
-              </li>
-          </ul>
-        </div>
-      </li>
-      <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
-        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
-          <i><img style="width:25px" src="{{ asset('material') }}/img/laravel.svg"></i>
-          <p>{{ __('Edit') }}
-            <b class="caret"></b>
-          </p>
-        </a>
-        <div class="collapse show" id="laravelExample">
-          <ul class="nav">
-            <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('profile.edit') }}">
-                <span class="sidebar-normal">{{ __('Terms') }} </span>
-              </a>
+                <div class="collapse show" id="laravelExample">
+                    <ul class="nav">
+                        <li class="nav-item{{ $activePage == 'financial-statement' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('profile.edit') }}">
+                                <span class="sidebar-normal">{{ __('Financial Statement') }} </span>
+                            </a>
+                        </li>
+                        <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('user.index') }}">
+                                <span class="sidebar-normal"> {{ __('User Management') }} </span>
+                            </a>
+                        </li>
+                        <li class="nav-item{{ $activePage == 'delivery-companies' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('admin.delivery') }}">
+                                <span class="sidebar-normal"> {{ __('Delivery Companies') }} </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
-            <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('user.index') }}">
-                <span class="sidebar-normal"> {{ __('Privacy Policy') }} </span>
-              </a>
+            <li class="nav-item {{ $activePage == 'profile' || $activePage == 'user-management' ? ' active' : '' }}">
+                <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
+                    <i><img style="width:25px" src="{{ asset('material') }}/img/laravel.svg"></i>
+                    <p>{{ __('Edit') }}
+                        <b class="caret"></b>
+                    </p>
+                </a>
+                <div class="collapse show" id="laravelExample">
+                    <ul class="nav">
+                        <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('profile.edit') }}">
+                                <span class="sidebar-normal">{{ __('Terms') }} </span>
+                            </a>
+                        </li>
+                        <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('user.index') }}">
+                                <span class="sidebar-normal"> {{ __('Privacy Policy') }} </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
-          </ul>
-        </div>
-      </li>
 
-      <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
-        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
-          <i><img style="width:25px" src="{{ asset('material') }}/img/laravel.svg"></i>
-          <p>{{ __('Add') }}
-            <b class="caret"></b>
-          </p>
-        </a>
-        <div class="collapse show" id="laravelExample">
-          <ul class="nav">
-            <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('profile.edit') }}">
-                <span class="sidebar-normal">{{ __('New Catogery') }} </span>
-              </a>
+            <li class="nav-item {{ $activePage == 'profile' || $activePage == 'user-management' ? ' active' : '' }}">
+                <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
+                    <i><img style="width:25px" src="{{ asset('material') }}/img/laravel.svg"></i>
+                    <p>{{ __('Add') }}
+                        <b class="caret"></b>
+                    </p>
+                </a>
+                <div class="collapse show" id="laravelExample">
+                    <ul class="nav">
+                        <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('profile.edit') }}">
+                                <span class="sidebar-normal">{{ __('New Catogery') }} </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
-          </ul>
-        </div>
-      </li>
 
-      <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
-        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
-          <i><img style="width:25px" src="{{ asset('material') }}/img/laravel.svg"></i>
-          <p>{{ __('Message') }}
-            <b class="caret"></b>
-          </p>
-        </a>
-        <div class="collapse show" id="laravelExample">
-          <ul class="nav">
-            <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
-              <a class="nav-link" href="{{ route('profile.edit') }}">
-                <span class="sidebar-normal">{{ __('Contact with Supervisor') }} </span>
-              </a>
+            <li class="nav-item {{ $activePage == 'profile' || $activePage == 'user-management' ? ' active' : '' }}">
+                <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
+                    <i><img style="width:25px" src="{{ asset('material') }}/img/laravel.svg"></i>
+                    <p>{{ __('Message') }}
+                        <b class="caret"></b>
+                    </p>
+                </a>
+                <div class="collapse show" id="laravelExample">
+                    <ul class="nav">
+                        <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('profile.edit') }}">
+                                <span class="sidebar-normal">{{ __('Contact with Supervisor') }} </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
-          </ul>
-        </div>
-      </li>
-
-
-    </ul>
-  </div>
-
+        </ul>
+    </div>
+</div>
