393a8ebcd83791a9ec1c2aadc87fb0d43c4a11c6
nasser650 <85414269+nasser650@users.noreply.github.com>
2021-11-14 01:58:27 +0300

    delivery

--- resources/views/admin/dashbord/delivery.blade.php
+++ resources/views/admin/dashbord/delivery.blade.php
@@ -1,5 +1,48 @@
-@extends('layouts.app', ['activePage' => 'delivery-company', 'titlePage' => 'Teststs'])
+@extends('layouts.app', ['activePage' => 'delivery-company', 'titlePage' => 'delivery company'])
 
 @section('content')
-    teste
+    <div class="content">
+        <div class="container-fluid">
+            <div class="container-fluid">
+                <div class="card card-plain">
+                    <div class="card-header card-header-primary">
+                        <h1 style="text-align:center">Delivery Companies</h1>
+                        <h3 style="text-align:center">Transport company data</h3>
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card-body">
+                                <div class="iframe-container d-none d-lg-block">
+
+                                    <p>
+                                    <h3 style="text-align:center">Delivery Companies is : </h3>
+                                    <h3 style="text-align:center">>
+                                        <select>
+                                            <option value="Select">Select</option>
+                                            <option value="Vineet">company-1</option>
+                                            <option value="Sumit">company-2</option>
+                                            <option value="Dorilal">company-3</option>
+                                        </select>
+                                        </p>
+
+                                        <form>
+                                            <h2> To add another company, please click here : <input type="submit" name="add"
+                                                    class="button" value="add" />
+
+                                            </h2>
+                                            <h2>To delete a company, please click here : <input type="submit" name="delet"
+                                                    class="button" value="delet" />
+                                        </form>
+
+                                        </h2>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
