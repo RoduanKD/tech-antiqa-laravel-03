6043a3ea802990b4d0864bcc9a9fb8761a45463b
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2021-11-14 06:23:35 +0200

    CRUD user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,4 +18,20 @@ class UserController extends Controller
     {
         return view('users.index', ['users' => $user]);
     }
+    public function create(User $user)
+    {
+        return view('users.create', [['users' => $user]]);
+    }
+    public function insert(Type $var = null)
+    {
+        # code...
+    }
+    public function edit(Type $var = null)
+    {
+        # code...
+    }
+    public function update(Type $var = null)
+    {
+        # code...
+    }
 }
