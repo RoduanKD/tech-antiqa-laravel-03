d780339335884ff11255f218390386fc1f9e68a6
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2021-11-14 11:27:14 +0200

    complet CRED user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,12 +25,22 @@ class UserController extends Controller
     }
     public function insert(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string|min:10|max:55',
+            'email' => 'required|email',
+            'phone' => 'required|numeric|min:8',
+            'birthdate' => 'required|date',
+            'password' => 'required|password',
+
+        ]);
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
         $user->phone = $request->phone;
         $user->birthdate = $request->birthdate;
         $user->password = $request->password;
+        $user->save();
+        return view('users.index');
     }
     public function edit(Type $var = null)
     {
